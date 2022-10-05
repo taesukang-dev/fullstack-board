e69c18ea4f44644812024fe93d74c9210263db63
mtae616 <mtae616@naver.com>
2022-10-05 12:47:09 +0900

    modify userdto

--- server/src/main/java/com/example/board/dto/UserDto.java
+++ server/src/main/java/com/example/board/dto/UserDto.java
@@ -14,9 +14,10 @@ import lombok.NoArgsConstructor;
 public class UserDto {
     private Long id;
     private String username;
+    private String password;
     private UserRole role;
 
     public static UserDto fromUser(User user) {
-        return new UserDto(user.getId(), user.getUsername(), user.getRole());
+        return new UserDto(user.getId(), user.getPassword(), user.getUsername(), user.getRole());
     }
 }
