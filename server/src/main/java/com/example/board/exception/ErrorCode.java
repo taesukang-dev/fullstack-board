dc2ca0de4989093de4d3a242f8cd1c30d5d1e8a7
mtae616 <mtae616@naver.com>
2022-10-05 23:01:50 +0900

    add errorcodes

--- server/src/main/java/com/example/board/exception/ErrorCode.java
+++ server/src/main/java/com/example/board/exception/ErrorCode.java
@@ -8,10 +8,12 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not found"),
     DUPLICATED_USER(HttpStatus.CONFLICT, "Already used username"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad request"),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Invalid password")
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "Invalid password"),
+    UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "Unauthorized access"),
     ;
 
     private HttpStatus status;
