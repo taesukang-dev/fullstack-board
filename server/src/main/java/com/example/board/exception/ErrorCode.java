eebef5da341c4c29a69e5b333d22841147050e8b
mtae616 <mtae616@naver.com>
2022-10-06 21:53:38 +0900

    fix error code

--- server/src/main/java/com/example/board/exception/ErrorCode.java
+++ server/src/main/java/com/example/board/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "Post not found"),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Comment not found"),
     DUPLICATED_USER(HttpStatus.CONFLICT, "Already used username"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad request"),
