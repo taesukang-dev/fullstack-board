3d42eae3249cb54c814b5820062ffc9ac9943d00
mtae616 <mtae616@naver.com>
2022-10-10 22:14:06 +0900

    add post component

---
+++ client/src/component/post/Post.style.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const GridBox = styled.td`
+    display: flex;
+    align-items: left;
+    justify-content: center;
+    flex-direction: column;
+    box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px;
+    padding: 16px;
+`
+
+export const TitleBox = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
