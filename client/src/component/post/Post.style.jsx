f5135a254a2592f66383b67e6267a96ad89aae09
mtae616 <mtae616@naver.com>
2022-10-11 15:54:13 +0900

    add login page

--- client/src/component/post/Post.style.jsx
+++ client/src/component/post/Post.style.jsx
@@ -1,16 +1,8 @@
 import styled from "styled-components";
 
-export const GridBox = styled.td`
-    display: flex;
-    align-items: left;
-    justify-content: center;
-    flex-direction: column;
+export const GridBox = styled.div`
+    display: grid;
+    grid-template-columns: 0.5fr 1fr 2fr 1fr;
     box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px;
     padding: 16px;
-`
-
-export const TitleBox = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
 `
