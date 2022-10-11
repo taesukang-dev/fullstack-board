fa4afb55f32e460a0c1e9497bc26aa39379da2db
mtae616 <mtae616@naver.com>
2022-10-11 15:52:53 +0900

    add sign in-up pathes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import {Route, Routes} from 'react-router-dom'
 import Main from "./pages/main/Main";
 import Header from "./component/header/Header";
+import SignUp from "./pages/signup/SignUp";
+import Login from "./pages/login/Login";
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
+import SignedHeader from "./component/header/SignedHeader";
 
 function App() {
+    const name = useSelector((state) => state.user)
+    useEffect(() => {
+        console.log(name)
+    },[name])
+
     return (
         <>
-            <Header/>
+            {name.current ? <SignedHeader /> : <Header />}
             <Routes>
-                <Route path={"/"} element={<Main/>}/>
+                <Route path={"/"} element={<Main />}/>
+                <Route path={"/signup"} element={<SignUp />} />
+                <Route path={"/login"} element={<Login />} />
             </Routes>
         </>
     );
