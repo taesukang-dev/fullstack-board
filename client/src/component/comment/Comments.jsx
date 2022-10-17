b2114b1e5aea2b1881e6f101c4d30f52b3738fdd
mtae616 <mtae616@naver.com>
2022-10-17 20:39:37 +0900

    add signed and unsigned comment

--- client/src/component/comment/Comments.jsx
+++ client/src/component/comment/Comments.jsx
@@ -1,25 +1,9 @@
 import * as s from './Comment.style'
-import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
-import {getComment, writeComment} from "../../shared/api/api";
 import {useState} from "react";
 import Text from "../../element/Text";
-import {useSelector} from "react-redux";
-import Button from "../../element/Button";
-import Comment from "./Comment";
+import CommentContainer from "./CommentContainer";
 
 const Comments = ({postId}) => {
-    const user = useSelector((state) => state.user)
-    const queryClient = useQueryClient()
-    let [comment, setComment] = useState([])
-
-    let result = useQuery(['comments'], () => getComment(postId))
-
-    let commentWriteMutation = useMutation(() => writeComment(postId, comment), {
-        onSuccess: (data) => {
-            queryClient.invalidateQueries(['comments'])
-        }
-    })
-
     return (
         <>
             <s.CommentTitleBox>
@@ -29,14 +13,7 @@ const Comments = ({postId}) => {
                 <div>작성자</div>
                 <div>내용</div>
             </s.GridBox>
-            <s.TypeBox>
-                <div>{user.current}</div>
-                <s.InputBox onChange={(e) => setComment(e.target.value)}/>
-                <Button _onClick={() => commentWriteMutation.mutate()}>작성하기</Button>
-            </s.TypeBox>
-            {
-                result.data?.result.map((e, i) => <Comment comment={e} key={i}/>)
-            }
+            <CommentContainer postId={postId} />
         </>
     )
 }
