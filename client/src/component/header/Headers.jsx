a7c457d6b3fd3c38d735654f8483d5f3a8f6b0ad
mtae616 <mtae616@naver.com>
2022-10-18 15:48:39 +0900

    fix client/reissue api

--- client/src/component/header/Headers.jsx
+++ client/src/component/header/Headers.jsx
@@ -2,16 +2,16 @@ import {useDispatch, useSelector} from "react-redux";
 import SignedHeader from "./SignedHeader";
 import Header from "./Header";
 import {useQuery} from "@tanstack/react-query";
-import {reissue, userInfo} from "../../shared/api/api";
+import {userInfo} from "../../shared/api/api";
 import {setUserUp} from "../../store/userSlice";
-import {useEffect} from "react";
 
 const Headers = () => {
     const dispatch = useDispatch()
-
     const name = useSelector((state) => state.user)
+
     useQuery(["username"], () => userInfo(), {
         onSuccess: (data) => dispatch(setUserUp(data.result.username)),
+        retry: 1,
         enabled: !!document.cookie.match('(^|;) ?' + 'x_auth' + '=([^;]*)(;|$)') && name.current === ''
     })
 
