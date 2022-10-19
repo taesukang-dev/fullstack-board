4fcc87e1d56d73e37d8a6c777912a065ae0c2dca
mtae616 <mtae616@naver.com>
2022-10-19 17:37:11 +0900

    add chatroom repository

---
+++ server/src/main/java/com/example/board/repository/ChatRoomRepository.java
@@ -0,0 +1,34 @@
+package com.example.board.repository;
+
+import com.example.board.dto.chat.ChatRoom;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@Repository
+public class ChatRoomRepository {
+
+    private Map<String, ChatRoom> chatRoomMap;
+
+    @PostConstruct
+    public void init() {
+        chatRoomMap = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        List chatRooms = new ArrayList<>(chatRoomMap.values());
+        Collections.reverse(chatRooms);
+        return chatRooms;
+    }
+
+    public ChatRoom findRoomById(String id) {
+        return chatRoomMap.get(id);
+    }
+
+    public ChatRoom createChatRoom(String name) {
+        ChatRoom chatRoom = ChatRoom.of(name);
+        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
+}
