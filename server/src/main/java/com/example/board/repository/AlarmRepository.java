f21036cf7121f52351eaf8a55443b38cbbcbe1e4
mtae616 <mtae616@naver.com>
2022-10-20 17:41:31 +0900

    add alarm repository

---
+++ server/src/main/java/com/example/board/repository/AlarmRepository.java
@@ -0,0 +1,29 @@
+package com.example.board.repository;
+
+import com.example.board.domain.Alarm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class AlarmRepository {
+    private final EntityManager em;
+
+    public void save(Alarm alarm) {
+        em.persist(alarm);
+    }
+
+    public Optional<List<Alarm>> findByUsername(String username) {
+        return Optional.ofNullable(em.createQuery("select a from Alarm a " +
+                        " join fetch a.receivedUser" +
+                        " where a.receivedUser.username=:username", Alarm.class)
+                .setParameter("username", username)
+                .getResultList());
+    }
+
+
+}
